9612745cce084376cb1de4c388a92fcbbc3143b4
Chrisaeus <87839602+Chrisaeus@users.noreply.github.com>
2021-11-02 20:14:07 -0700

    added functionality for first three qs, app loops

--- index.js
+++ index.js
@@ -11,27 +11,68 @@ const db = mysql.createConnection(
     console.log(`Connected to MU/TH/UR.`)
 );
 
-inquirer
-    .prompt(
-        {
-            type: "list",
-            name: "mainMenu",
-            message: "What would you like to do?",
-            choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"],
-        }
-    )
-    .then(response => {
-        //  if (response.mainMenu == "View all departments") {
+const mainMenu = () => {
+    inquirer
+        .prompt(
+            {
+                type: "list",
+                name: "mainMenu",
+                message: "What would you like to do?",
+                choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"],
+            }
+        )
+        .then(response => {
+            //  if (response.mainMenu == "View all departments") {
 
-        //  } else if ()
-        switch (response.mainMenu) {
-            case "View all departments":
-                db.query("SELECT * FROM department", (err, result) => {
-                    err ? console.log(err) : console.table(result);
-                });
-                break;
-            default:
-                console.log("Choose an option");
-        }
-    })
-    .catch(err => console.log(err));
+            //  } else if ()
+            switch (response.mainMenu) {
+                case "View all departments":
+                    db.query("SELECT * FROM department", (err, result) => {
+                        err ? console.log(err) : console.table(result);
+                        mainMenu();
+                    });
+                    break;
+                case "View all roles":
+                    db.query("SELECT * FROM role", (err, result) => {
+                        err ? console.log(err) : console.table(result);
+                        mainMenu();
+                    });
+                    break;
+                case "View all employees":
+                    db.query("SELECT * FROM employee", (err, result) => {
+                        err ? console.log(err) : console.table(result);
+                        mainMenu();
+                    });
+                    break;
+                case "Add a department":
+                    db.query("", (err, result) => {
+
+                        mainMenu();
+                    });
+                    break;
+                case "Add a role":
+                    db.query("", (err, result) => {
+
+                        mainMenu();
+                    });
+                    break;
+                case "Add an employee":
+                    db.query("", (err, result) => {
+
+                        mainMenu();
+                    });
+                    break;
+                case "Update an employee role":
+                    db.query("", (err, result) => {
+
+                        mainMenu();
+                    });
+                    break;
+                default:
+                    mainMenu();
+            }
+        })
+        .catch(err => console.log(err));
+}
+
+mainMenu();
